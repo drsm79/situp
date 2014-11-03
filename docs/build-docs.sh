36f15ab2c471af6d17139c804c8c59aacd51ec82
BigBlueHat <byoung@bigbluehat.com>
2014-11-03 15:51:00 -0500

    Fix build-docs.sh command list output

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -1,7 +1,7 @@
-#!/usr/env sh
+#!/usr/bin/env sh
 
 # build the CLI docs - dump into cli.rst
-for cmd in `./situp.py|head -n2|tail -n1|awk -F : '{print $2}'`;
+for cmd in `../situp.py|head -n2|tail -n1|awk -F : '{print $2}'`;
    do echo $cmd;
 done
 # run sphinx, writing to tmp
